f3a2814a5668d6b98b2bf173d221a14add5d3492
Amethyste34 <laurence.amethyste@gmail.com>
2025-02-16 17:20:15 +0100

    Réalisation première carte : pikachu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PlayingCardComponent } from "./components/playing-card/playing-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [PlayingCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
